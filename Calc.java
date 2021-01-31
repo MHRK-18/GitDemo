807f15d465b9e743c4af1fb25fb119644ad1b295
MHRK-18 <71684561+MHRK-18@users.noreply.github.com>
2021-01-31 15:37:04 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,25 +1,45 @@
 import java.util.Scanner;
- 
-public class JavaProgram
+public class AddTwoNumbers
 {
-    public static void main(String args[])
-  {
-    int first, second, add, subtract, multiply;
-    float divide;
-    Scanner in = new Scanner(System.in);
+       public static float add(float a, float b)
+   {
+      float sum = a + b;
+      return sum;
+   }
+      public static float sub(float a, float b)
+   {
+      float dif= a - b;
+      return dif;
+   }
+      public static float mul(float a, float b)
+   {
+      float pro= a * b;
+      return pro;
+   }
+      public static float div(float a, float b)
+   {
+      float div;
+      div=a>b?a/b:b/a;
+      return div;
+   }
+   
+   public static void main(String[] args)
+   {
+      float num1, num2, s,d,m,di;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("First number : ");
+      num1 = sc.nextInt();
+      System.out.println("Second number : ");
+      num2 = sc.nextInt();
+      s = add(num1, num2);
+      System.out.println("Sum : " + s);
+    d= sub(num1, num2);
+      System.out.println("Difference : " + d);
+    m = mul(num1, num2);
+      System.out.println("Product : " + m);
+        di = div(num1, num2);
+      System.out.println("Division : " + di);
+      sc.close();
+   }
 
-    System.out.print("Enter Two Numbers : ");
-    first=in.nextInt();
-    second=in.nextInt();
-
-    add = first + second;
-    subtract = first - second;
-    multiply = first * second;
-    divide = (float) first / second;
-
-    System.out.println("Sum = " + add);
-    System.out.println("Difference = " + subtract);
-    System.out.println("Multiplication = " + multiply);
-    System.out.println("Division = " + divide);
-  }
 }
